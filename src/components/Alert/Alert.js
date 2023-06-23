4e9cf178f367cfe06adf20b1be3098b939efc46c
hasyim404 <muhaisim7@gmail.com>
2023-06-23 16:33:52 +0700

    fix abnormal form input and warning on console

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,16 +1,17 @@
 import styled from "styled-components";
 
+const StyledAlert = styled.div`
+  margin-top: -0.5rem;
+  margin-bottom: 0.5rem;
+
+  .alert {
+    color: rgb(216, 23, 23);
+    font-style: italic;
+  }
+`;
+
 function Alert(props) {
   const { children } = props;
-  const StyledAlert = styled.div`
-    margin-top: -0.5rem;
-    margin-bottom: 0.5rem;
-
-    .alert {
-      color: rgb(216, 23, 23);
-      font-style: italic;
-    }
-  `;
 
   return (
     <StyledAlert>
--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,24 +1,25 @@
 import styled from "styled-components";
 
+const StyledContainer = styled.div`
+  /* Small Screen */
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 1rem;
+
+  @media (min-width: 768px) {
+    /* 
+      * Nothing change for now
+      */
+  }
+
+  @media (min-width: 992px) {
+    /* 
+      * Nothing change for now
+      */
+  }
+`;
+
 function Container(props) {
-  const StyledContainer = styled.div`
-    /* Small Screen */
-    max-width: 1200px;
-    margin: 0 auto;
-    padding: 1rem;
-
-    @media (min-width: 768px) {
-      /* 
-      * Nothing change for now
-      */
-    }
-
-    @media (min-width: 992px) {
-      /* 
-      * Nothing change for now
-      */
-    }
-  `;
   return (
     <>
       <StyledContainer>{props.children}</StyledContainer>
