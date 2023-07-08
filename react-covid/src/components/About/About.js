f9185684530542e7ce18dfde8aedf4313939e96c
hasyim404 <muhaisim7@gmail.com>
2023-07-08 23:10:08 +0700

    update about

--- react-covid/src/components/About/About.js
+++ react-covid/src/components/About/About.js
@@ -1,9 +1,15 @@
-import Hero from "../Hero/Hero";
+import { Heading } from "../ui/Typography";
 
 function AboutPage() {
   return (
     <div>
-      <Hero />;
+      <Heading
+        fontColor="secondary"
+        style={{ textAlign: "center", margin: "10rem" }}
+      >
+        Baru pertama kali belajar react.js, pengen bisa lebih memahami lagi
+        terutama dalam thingking in react T_T
+      </Heading>
     </div>
   );
 }
